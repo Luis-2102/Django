b2f55f8406dc1dc152ed1703a0fc17b8ed46a103
Luis-2102 <143152087+Luis-2102@users.noreply.github.com>
2024-01-31 19:28:47 -0500

    Update settings.py

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -81,18 +81,8 @@ WSGI_APPLICATION = 'webplayground.wsgi.application'
 
 DATABASES = {
     'default': {
-
-        "ENGINE": "django.db.backends.postgresql",
-
-        "NAME": 'renta',
-
-        "USER": 'postgres',
-
-        "PASSWORD": 'admin12',
-
-        "HOST": 'localhost',
-
-        "PORT": 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
